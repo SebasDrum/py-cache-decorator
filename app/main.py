f0c16396e0d5ad6aa9442da45fabc27524741935
Sebastián A. Estrada <sestrada@feralocal.com>
2026-05-28 16:52:55 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,20 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    
+    memoria = {}
+    
+    def wrapper(*args, **kwargs):
+        # Creamos una clave única que soporte argumentos posicionales y nombrados
+        clave = (args, tuple(sorted(kwargs.items())))
+        
+        if clave in memoria:
+            print("Getting from cache")
+            return memoria[clave]
+        
+        print("Calculating new result")
+        resultado = func(*args, **kwargs)
+        memoria[clave] = resultado
+        return resultado
+            
+    return wrapper
